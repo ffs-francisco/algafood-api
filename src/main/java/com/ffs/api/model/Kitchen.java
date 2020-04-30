af9c7a410c282938c87e50edb9435795fab471ee
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-04-30 14:23:37 -0300

    Configure application to create database tables from entities

---
+++ src/main/java/com/ffs/api/model/Kitchen.java
@@ -0,0 +1,62 @@
+package com.ffs.api.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author francisco
+ */
+@Entity
+public class Kitchen implements Serializable {
+
+    @Id
+    private Long Id;
+    private String name;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 11 * hash + Objects.hashCode(this.Id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Kitchen other = (Kitchen) obj;
+        return Objects.equals(this.Id, other.Id);
+    }
+
+    @Override
+    public String toString() {
+        return "Kitchen{" + "Id=" + Id + ", name=" + name + '}';
+    }
+
+}
---
+++ src/main/java/com/ffs/api/model/Restaurant.java
@@ -0,0 +1,71 @@
+package com.ffs.api.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author francisco
+ */
+@Entity
+public class Restaurant implements Serializable {
+
+    @Id
+    private Long id;
+    private String name;
+    private BigDecimal shippingFee;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getShippingFee() {
+        return shippingFee;
+    }
+
+    public void setShippingFee(BigDecimal shippingFee) {
+        this.shippingFee = shippingFee;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Restaurant other = (Restaurant) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" + "id=" + id + ", name=" + name + ", shippingFee=" + shippingFee + '}';
+    }
+}
