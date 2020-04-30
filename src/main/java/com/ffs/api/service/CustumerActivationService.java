4cba86be815a60cd2bd28e4a8fc269814b751a49
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-04-29 22:12:51 -0300

    Created callback methods in bean life cycle

--- src/main/java/com/ffs/api/service/CustumerActivationService.java
+++ src/main/java/com/ffs/api/service/CustumerActivationService.java
@@ -3,6 +3,8 @@ package com.ffs.api.service;
 import com.ffs.api.model.Custumer;
 import com.ffs.api.notification.Notifier;
 import com.ffs.api.notification.type.NotifierType;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -22,6 +24,16 @@ public class CustumerActivationService {
             @Autowired Notifier notifiers
     ) {
         this.notifier = notifiers;
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Initialized!");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Stopped!");
     }
 
     public void activate(final Custumer custumer) {
