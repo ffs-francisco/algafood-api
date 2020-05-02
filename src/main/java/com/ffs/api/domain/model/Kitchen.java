14eda4858ac0dea9af091efa884c77d1e19713be
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-05-02 00:39:26 -0300

    Using Lombok project

--- src/main/java/com/ffs/api/domain/model/Kitchen.java
+++ src/main/java/com/ffs/api/domain/model/Kitchen.java
@@ -1,10 +1,11 @@
 package com.ffs.api.domain.model;
 
 import java.io.Serializable;
-import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -12,55 +13,15 @@ import static javax.persistence.GenerationType.IDENTITY;
  *
  * @author francisco
  */
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Kitchen implements Serializable {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long Id;
+
     private String name;
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long Id) {
-        this.Id = Id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 11 * hash + Objects.hashCode(this.Id);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Kitchen other = (Kitchen) obj;
-        return Objects.equals(this.Id, other.Id);
-    }
-
-    @Override
-    public String toString() {
-        return "Kitchen{" + "Id=" + Id + ", name=" + name + '}';
-    }
-
 }
--- src/main/java/com/ffs/api/domain/model/Restaurant.java
+++ src/main/java/com/ffs/api/domain/model/Restaurant.java
@@ -2,10 +2,11 @@ package com.ffs.api.domain.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -13,63 +14,17 @@ import static javax.persistence.GenerationType.IDENTITY;
  *
  * @author francisco
  */
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurant implements Serializable {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
     private String name;
+
     private BigDecimal shippingFee;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getShippingFee() {
-        return shippingFee;
-    }
-
-    public void setShippingFee(BigDecimal shippingFee) {
-        this.shippingFee = shippingFee;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 67 * hash + Objects.hashCode(this.id);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Restaurant other = (Restaurant) obj;
-        return Objects.equals(this.id, other.id);
-    }
-
-    @Override
-    public String toString() {
-        return "Restaurant{" + "id=" + id + ", name=" + name + ", shippingFee=" + shippingFee + '}';
-    }
 }
