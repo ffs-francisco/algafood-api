129fd96cc62656e6ba938af3360ebbf0c277e88e
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-05-03 17:37:42 -0300

    Adding listing of kitchens resources

---
+++ src/main/java/com/ffs/api/controller/KitchenController.java
@@ -0,0 +1,26 @@
+package com.ffs.api.controller;
+
+import com.ffs.api.domain.model.Kitchen;
+import com.ffs.api.domain.repository.KitchenRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author francisco
+ */
+@RestController
+@RequestMapping("/kitchens")
+public class KitchenController {
+
+    @Autowired
+    private KitchenRepository kitchenRepository;
+
+    @GetMapping
+    public List<Kitchen> listAll() {
+        return this.kitchenRepository.findAll();
+    }
+}
