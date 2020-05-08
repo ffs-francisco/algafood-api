ea53ef0b701bc04f5de4fadad1beba13467e68f6
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-05-07 21:04:18 -0300

    Implementation the update of restaurant resource

--- src/main/java/com/ffs/api/controller/RestaurantConstroller.java
+++ src/main/java/com/ffs/api/controller/RestaurantConstroller.java
@@ -5,12 +5,14 @@ import com.ffs.api.domain.model.Restaurant;
 import com.ffs.api.domain.repository.RestaurantRepository;
 import com.ffs.api.domain.service.RestaurantRegistrationService;
 import java.util.List;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,6 +51,24 @@ public class RestaurantConstroller {
     try {
       return ResponseEntity.status(HttpStatus.CREATED)
               .body(restaurantRegistrationService.save(restaurant));
+    } catch (EntityNotFoundException e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+
+  @PutMapping("/{resturantId}")
+  public ResponseEntity<?> update(@PathVariable final Long resturantId, @RequestBody final Restaurant restaurant) {
+    var restaurantSaved = restaurantRepository.findById(resturantId);
+
+    try {
+      if (restaurantSaved != null) {
+        BeanUtils.copyProperties(restaurant, restaurantSaved, "id");
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(restaurantRegistrationService.save(restaurantSaved));
+      }
+
+      return ResponseEntity.notFound().build();
     } catch (EntityNotFoundException e) {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
