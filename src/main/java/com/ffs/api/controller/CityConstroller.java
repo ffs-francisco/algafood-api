9fd1b8ad2aaee957825d245dd809043e1725acad
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-05-08 01:14:13 -0300

    Implementing the  city resource updating.

--- src/main/java/com/ffs/api/controller/CityConstroller.java
+++ src/main/java/com/ffs/api/controller/CityConstroller.java
@@ -5,12 +5,14 @@ import com.ffs.api.domain.model.City;
 import com.ffs.api.domain.repository.CityRepository;
 import com.ffs.api.domain.service.CityRegistrationService;
 import java.util.List;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,6 +51,23 @@ public class CityConstroller {
         try {
             return ResponseEntity.status(HttpStatus.CREATED)
                     .body(cityRegistrationService.save(city));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{cityId}")
+    public ResponseEntity<?> update(@PathVariable final Long cityId, @RequestBody City city) {
+        final var citySaved = cityRepository.findById(cityId);
+
+        try {
+            if (citySaved != null) {
+                BeanUtils.copyProperties(city, citySaved, "id");
+
+                return ResponseEntity.ok(cityRegistrationService.save(citySaved));
+            }
+
+            return ResponseEntity.notFound().build();
         } catch (EntityNotFoundException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
