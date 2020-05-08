28ca8d55f0d78c5aba894f09703b69b1325a5db8
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-05-07 21:04:18 -0300

    Implementation the state resource search by id

--- src/main/java/com/ffs/api/controller/StateController.java
+++ src/main/java/com/ffs/api/controller/StateController.java
@@ -4,7 +4,9 @@ import com.ffs.api.domain.model.State;
 import com.ffs.api.domain.repository.StateRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,15 @@ public class StateController {
     @GetMapping
     public List<State> listAll() {
         return this.stateRepository.findAll();
+    }
+
+    @GetMapping("/{stateId}")
+    public ResponseEntity<State> getById(@PathVariable Long stateId) {
+        var state = stateRepository.findById(stateId);
+        if (state == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(state);
     }
 }
