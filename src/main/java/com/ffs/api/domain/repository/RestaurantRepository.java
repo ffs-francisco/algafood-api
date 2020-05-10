a1d7b52aac10cbf15c179cbaa7ea3b824f418873
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-05-10 00:15:03 -0300

    Using custom JPQL queries with the annotation @Query

--- src/main/java/com/ffs/api/domain/repository/RestaurantRepository.java
+++ src/main/java/com/ffs/api/domain/repository/RestaurantRepository.java
@@ -5,6 +5,8 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -14,13 +16,14 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
-    List<Restaurant> findByShippingFeeBetween(BigDecimal shippingFeeInitial, BigDecimal shippingFeeFinal);
-
-    List<Restaurant> findByNameContainingAndKitchenId(String name, Long kitchenId);
+    int countByKitchenId(Long kitchenId);
 
     Optional<Restaurant> findFirstByNameContainingAndKitchenId(String name, Long kitchenId);
 
+    List<Restaurant> findByShippingFeeBetween(BigDecimal shippingFeeInitial, BigDecimal shippingFeeFinal);
+
     List<Restaurant> findTop2ByNameContainingAndKitchenId(String name, Long kitchenId);
 
-    int countByKitchenId(Long kitchenId);
+    @Query("FROM Restaurant WHERE name LIKE %:name% AND kitchen.id = :kitchenId")
+    List<Restaurant> findByLikeNameAndKitchenId(@Param("name") String name, @Param("kitchenId") Long kitchenId);
 }
