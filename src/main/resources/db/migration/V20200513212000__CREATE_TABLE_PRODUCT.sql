acf126a87cf9cd217b2926d308fd0989a86823c5
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-05-13 21:57:49 -0300

    Adding migration to create table of Product

---
+++ src/main/resources/db/migration/V20200513212000__CREATE_TABLE_PRODUCT.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS product (
+  id            BIGINT        NOT NULL AUTO_INCREMENT,
+  name          VARCHAR(100)  NOT NULL,
+  description   VARCHAR(100)  NOT NULL,
+  price         DECIMAL(19,2) NOT NULL,
+  active        BIT(1)        NOT NULL,
+
+  restaurant_id  BIGINT DEFAULT NULL,   
+
+  PRIMARY KEY (id),
+  CONSTRAINT fk_p_restaurant_id FOREIGN KEY (restaurant_id) REFERENCES restaurant (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
