f6200c5616e1876d839ee6c31c30b569caeaba9e
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-05-18 19:59:11 -0300

    Using the RFC 7807 as base for format the body in the exceptions response

--- src/main/java/com/ffs/algafood/api/exception/ApiException.java
+++ src/main/java/com/ffs/algafood/api/exception/ApiException.java
@@ -13,16 +13,16 @@ import org.springframework.web.context.request.WebRequest;
 public class ApiException {
 
     private final LocalDateTime timestamp;
-    private final int status;
-    private final String error;
-    private final String message;
+    private final Integer status;
+    private final String title;
+    private final String detail;
     private final String path;
 
     public ApiException(String message, HttpStatus status, WebRequest request) {
         this.timestamp = LocalDateTime.now();
         this.status = status.value();
-        this.error = status.getReasonPhrase();
-        this.message = message;
+        this.title = status.getReasonPhrase();
+        this.detail = message;
         this.path = request.getDescription(false).substring(4);
     }
 }
