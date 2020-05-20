d9835ec8d9b7f06a403c5a3c91665d36f5d40085
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-05-20 00:39:30 -0300

    Treating errors when of missing and ignored properties in deserialization.

--- src/main/java/com/ffs/algafood/api/exception/ApiExceptionHandler.java
+++ src/main/java/com/ffs/algafood/api/exception/ApiExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.ffs.algafood.api.exception;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 import com.ffs.algafood.domain.exception.base.BusinessException;
 import com.ffs.algafood.domain.exception.base.EntityInUseException;
 import com.ffs.algafood.domain.exception.base.EntityNotFoundException;
@@ -27,50 +29,53 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<?> handlerNotFound(EntityNotFoundException ex, WebRequest request) {
-        var apiException = new ApiException(ex.getMessage(), ENTITY_NOT_FOUND, NOT_FOUND, request);
+        final var apiException = new ApiException(ex.getMessage(), ENTITY_NOT_FOUND, NOT_FOUND, request);
 
-        return this.handleExceptionInternal(ex, apiException, new HttpHeaders(), NOT_FOUND, request);
+        return super.handleExceptionInternal(ex, apiException, new HttpHeaders(), NOT_FOUND, request);
     }
 
     @ExceptionHandler(EntityInUseException.class)
     public ResponseEntity<?> handlerEntityInUseException(EntityInUseException ex, WebRequest request) {
-        var apiException = new ApiException(ex.getMessage(), ENTITY_IN_USE, CONFLICT, request);
+        final var apiException = new ApiException(ex.getMessage(), ENTITY_IN_USE, CONFLICT, request);
 
-        return this.handleExceptionInternal(ex, apiException, new HttpHeaders(), CONFLICT, request);
+        return super.handleExceptionInternal(ex, apiException, new HttpHeaders(), CONFLICT, request);
     }
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<?> handlerBusinessException(BusinessException ex, WebRequest request) {
-        var apiException = new ApiException(ex.getMessage(), ERROR_BUSINESS, BAD_REQUEST, request);
+        final var apiException = new ApiException(ex.getMessage(), ERROR_BUSINESS, BAD_REQUEST, request);
 
         return this.handleExceptionInternal(ex, apiException, new HttpHeaders(), BAD_REQUEST, request);
     }
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        var detail = "The request body is invalid. Verify syntax errors.";
+
         Throwable rootCause = ExceptionUtils.getRootCause(ex);
         if (rootCause instanceof InvalidFormatException) {
-            return this.handleInvalidFormatException((InvalidFormatException) rootCause, headers, BAD_REQUEST, request);
+            detail = this.detailInvalidFormatException((InvalidFormatException) rootCause);
+        } else if (rootCause instanceof PropertyBindingException) {
+            detail = this.detailPropertyBindingException((PropertyBindingException) rootCause);
         }
 
-        var detail = "The request body is invalid. Verify syntax errors.";
-        var apiException = new ApiException(detail, INCOMPREHENSIBLE_MESSAGE, BAD_REQUEST, request);
-
-        return this.handleExceptionInternal(ex, apiException, new HttpHeaders(), BAD_REQUEST, request);
+        final var apiException = new ApiException(detail, INCOMPREHENSIBLE_MESSAGE, BAD_REQUEST, request);
+        return super.handleExceptionInternal(ex, apiException, new HttpHeaders(), BAD_REQUEST, request);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleExceptionInternal(ex, body, headers, status, request);
+    private String detailInvalidFormatException(InvalidFormatException ex) {
+        return String.format("The property '%s' received a value of '%s', which is of type invalid. Enter a '%s' type value.",
+                this.joinPath(ex), ex.getValue(), ex.getTargetType().getSimpleName());
     }
 
-    private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        var detail = "The proprety '%s' received a value of '%s', which is of type invalid. Enter a '%s' type value.";
-        var path = ex.getPath().stream().map(ref -> ref.getFieldName()).collect(Collectors.joining("."));
+    private String detailPropertyBindingException(PropertyBindingException ex) {
+        return String.format("The property '%s' not exist. Correct or remove it and try again.",
+                this.joinPath(ex));
+    }
 
-        detail = String.format(detail, path, ex.getValue(), ex.getTargetType().getSimpleName());
-        var apiException = new ApiException(detail, INCOMPREHENSIBLE_MESSAGE, BAD_REQUEST, request);
-
-        return this.handleExceptionInternal(ex, apiException, new HttpHeaders(), BAD_REQUEST, request);
+    private String joinPath(MismatchedInputException ex) {
+        return ex.getPath().stream()
+                .map(reference -> reference.getFieldName())
+                .collect(Collectors.joining("."));
     }
 }
