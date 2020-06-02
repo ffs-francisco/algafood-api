8389b0879779bc7ff00f5617fb86d830f03513cd
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-06-02 02:18:22 -0300

    Adding new endpoints in payment method controller for add and update

--- src/main/java/com/ffs/algafood/api/controller/PaymentMethodController.java
+++ src/main/java/com/ffs/algafood/api/controller/PaymentMethodController.java
@@ -1,16 +1,22 @@
 package com.ffs.algafood.api.controller;
 
+import com.ffs.algafood.api.model.request.payment.method.PaymentMethodRequest;
 import com.ffs.algafood.api.model.response.payment.method.PaymentMethodResponse;
 import com.ffs.algafood.domain.service.PaymentMethodService;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.CREATED;
 
 /**
  *
@@ -33,5 +39,23 @@ public class PaymentMethodController {
     @ResponseStatus(OK)
     public PaymentMethodResponse getById(@PathVariable final Long paymentMethodId) {
         return PaymentMethodResponse.from(paymentService.findById(paymentMethodId));
+    }
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public PaymentMethodResponse add(@RequestBody @Valid final PaymentMethodRequest paymentMethodRequest) {
+        return PaymentMethodResponse.from(paymentService.save(paymentMethodRequest.toModel()));
+    }
+
+    @PutMapping("/{paymentMethodId}")
+    @ResponseStatus(OK)
+    public PaymentMethodResponse update(
+            @PathVariable final Long paymentMethodId,
+            @RequestBody @Valid final PaymentMethodRequest paymentMethodRequest
+    ) {
+        final var paymentMethodSaved = paymentService.findById(paymentMethodId);
+
+        paymentMethodRequest.copyPropertiesTo(paymentMethodSaved);
+        return PaymentMethodResponse.from(paymentService.save(paymentMethodSaved));
     }
 }
---
+++ src/main/java/com/ffs/algafood/api/model/request/payment/method/PaymentMethodRequest.java
@@ -0,0 +1,27 @@
+package com.ffs.algafood.api.model.request.payment.method;
+
+import com.ffs.algafood.domain.model.PaymentMethod;
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+
+/**
+ *
+ * @author francisco
+ */
+@Getter
+@Setter
+public class PaymentMethodRequest {
+
+    @NotBlank
+    private String description;
+
+    public PaymentMethod toModel() {
+        return new ModelMapper().map(this, PaymentMethod.class);
+    }
+
+    public void copyPropertiesTo(PaymentMethod paymentMethod) {
+        new ModelMapper().map(this, paymentMethod);
+    }
+}
