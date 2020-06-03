cf2ed06f2f305043bbe0b10f7c28965b410ce97e
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-06-03 01:19:22 -0300

    Adding new endpoints in group controller for add and update

--- src/main/java/com/ffs/algafood/api/controller/GroupController.java
+++ src/main/java/com/ffs/algafood/api/controller/GroupController.java
@@ -1,16 +1,22 @@
 package com.ffs.algafood.api.controller;
 
+import com.ffs.algafood.api.model.request.group.GroupRequest;
 import com.ffs.algafood.api.model.response.group.GroupResponse;
 import com.ffs.algafood.domain.service.GroupService;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.CREATED;
 
 /**
  *
@@ -33,5 +39,23 @@ public class GroupController {
     @ResponseStatus(OK)
     public GroupResponse getById(@PathVariable final Long groupId) {
         return GroupResponse.from(groupService.findById(groupId));
+    }
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public GroupResponse add(@RequestBody @Valid final GroupRequest groupRequest) {
+        return GroupResponse.from(groupService.save(groupRequest.toModel()));
+    }
+
+    @PutMapping("/{groupId}")
+    @ResponseStatus(OK)
+    public GroupResponse update(
+            @PathVariable final Long groupId,
+            @RequestBody @Valid final GroupRequest groupRequest
+    ) {
+        final var groupSaved = groupService.findById(groupId);
+
+        groupRequest.copyPropertiesTo(groupSaved);
+        return GroupResponse.from(groupService.save(groupSaved));
     }
 }
---
+++ src/main/java/com/ffs/algafood/api/model/request/group/GroupRequest.java
@@ -0,0 +1,27 @@
+package com.ffs.algafood.api.model.request.group;
+
+import com.ffs.algafood.domain.model.Group;
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+
+/**
+ *
+ * @author francisco
+ */
+@Getter
+@Setter
+public class GroupRequest {
+
+    @NotBlank
+    private String name;
+
+    public Group toModel() {
+        return new ModelMapper().map(this, Group.class);
+    }
+
+    public void copyPropertiesTo(final Group group) {
+        new ModelMapper().map(this, group);
+    }
+}
