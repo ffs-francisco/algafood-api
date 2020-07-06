bbf95ae409560eb4658835716f1e9b9edc61be89
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-07-06 01:32:20 -0300

    Adding custom serializer for Page

---
+++ src/main/java/com/ffs/algafood/core/jackson/PageJsonSerializer.java
@@ -0,0 +1,30 @@
+package com.ffs.algafood.core.jackson;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.data.domain.Page;
+
+/**
+ *
+ * @author francisco
+ */
+@JsonComponent
+public class PageJsonSerializer extends JsonSerializer<Page<?>> {
+
+    @Override
+    public void serialize(Page<?> page, JsonGenerator generator, SerializerProvider provider) throws IOException {
+        generator.writeStartObject();
+
+        generator.writeObjectField("content", page.getContent());
+
+        generator.writeObjectField("size", page.getSize());
+        generator.writeObjectField("number", page.getNumber());
+        generator.writeObjectField("totalPages", page.getTotalPages());
+        generator.writeObjectField("totalElements", page.getTotalElements());
+
+        generator.writeEndObject();
+    }
+}
