b949e21cc7be8b38b3e6ca4eeebc6f685018a9c7
Francisco Silva <ffsfranciscosilva@gmail.com>
2021-03-23 14:12:01 -0300

    Filter controlles by package

--- src/main/java/com/ffs/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/ffs/algafood/core/openapi/SpringFoxConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,7 +19,8 @@ public class SpringFoxConfig implements WebMvcConfigurer {
     public Docket apiDocket() {
         return new Docket(SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.ffs.algafood.api"))
+                .paths(PathSelectors.any())
                 .build();
     }
 
