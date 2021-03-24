8f27ebb83ad4bd70c2b613cf9300e5ac58129bb2
Francisco Silva <ffsfranciscosilva@gmail.com>
2021-03-23 22:35:46 -0300

    Adding global custom messages of http status code

--- src/main/java/com/ffs/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/ffs/algafood/core/openapi/SpringFoxConfig.java
@@ -9,12 +9,20 @@ import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_ACCEPTABLE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 @Configuration
@@ -29,17 +37,10 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 .apis(RequestHandlerSelectors.basePackage("com.ffs.algafood.api"))
                 .paths(PathSelectors.any())
                 .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(GET, getResponseMessages())
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cities", "Management os Cities"));
-    }
-
-    public ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("AlgaFood API")
-                .description("Open API for customers and restaurants")
-                .version("0.0.1-SNAPSHOT")
-                .contact(new Contact("AlgaFood", "https://www.algafood-domain.com", "contact@algafood-domain.com"))
-                .build();
     }
 
     @Override
@@ -49,5 +50,27 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("AlgaFood API")
+                .description("Open API for customers and restaurants")
+                .version("0.0.1-SNAPSHOT")
+                .contact(new Contact("AlgaFood", "https://www.algafood-domain.com", "contact@algafood-domain.com"))
+                .build();
+    }
+
+    private List<ResponseMessage> getResponseMessages() {
+        return Arrays.asList(
+                new ResponseMessageBuilder()
+                        .code(INTERNAL_SERVER_ERROR.value())
+                        .message("Internal server error")
+                        .build(),
+                new ResponseMessageBuilder()
+                        .code(NOT_ACCEPTABLE.value())
+                        .message("Representation not acceptable")
+                        .build()
+        );
     }
 }
