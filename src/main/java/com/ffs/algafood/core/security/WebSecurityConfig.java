f07b99635e3c477a9ffda31e9dc0945547414f11
Francisco Silva <ffsfranciscosilva@gmail.com>
2021-04-01 14:51:05 -0300

    Configure to user in memory authentication

--- src/main/java/com/ffs/algafood/core/security/WebSecurityConfig.java
+++ src/main/java/com/ffs/algafood/core/security/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package com.ffs.algafood.core.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -27,5 +31,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable()
         ;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Francisco").password(passwordEncoder().encode("algafood"))
+                .roles("ADMIN")
+                .and()
+                .withUser("Netto").password(passwordEncoder().encode("123"))
+                .roles("ADMIN")
+        ;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
