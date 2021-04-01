dce355d3efb595541f14f9c1b01e397ef22bffca
Francisco Silva <ffsfranciscosilva@gmail.com>
2021-04-01 16:59:43 -0300

    Adding base project for Authorization Server

--- algafood-auth/src/main/java/com/ffs/algafood/auth/AuthApplication.java
+++ algafood-auth/src/main/java/com/ffs/algafood/auth/AuthApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlgafoodAuthApplication {
+public class AuthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AlgafoodAuthApplication.class, args);
+        SpringApplication.run(AuthApplication.class, args);
     }
 }
