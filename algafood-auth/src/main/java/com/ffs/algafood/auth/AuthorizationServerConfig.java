6119ecdb2231d02347b0c018aef6f705ac4407ce
Francisco Silva <ffsfranciscosilva@gmail.com>
2021-04-01 17:30:40 -0300

    Configure the Authorzation Server flow with Passsword Credentials and Opaque Tokens

--- algafood-auth/src/main/java/com/ffs/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/ffs/algafood/auth/AuthorizationServerConfig.java
@@ -1,12 +1,43 @@
 package com.ffs.algafood.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    @Autowired
+    private PasswordEncoder encoder;
 
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("algafood-web")
+                .secret(encoder.encode("web-123"))
+                .authorizedGrantTypes("password")
+                .scopes("write", "read")
+                .and()
+                .withClient("algafood-app")
+                .secret(encoder.encode("web-123"))
+                .authorizedGrantTypes("password")
+                .scopes("write", "read")
+
+                .accessTokenValiditySeconds(60 * 60 * 6) // six hours
+        ;
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(manager);
+    }
 }
--- algafood-auth/src/main/java/com/ffs/algafood/auth/WebSecurityConfig.java
+++ algafood-auth/src/main/java/com/ffs/algafood/auth/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.ffs.algafood.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,6 +23,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("Netto").password(passwordEncoder().encode("123"))
                 .roles("ADMIN")
         ;
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
     @Bean
