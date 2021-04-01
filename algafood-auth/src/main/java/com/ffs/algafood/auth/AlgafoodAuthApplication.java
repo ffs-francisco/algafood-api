3e66f392b4f68da00a0065331dc28e9f51223c1d
Francisco Silva <ffsfranciscosilva@gmail.com>
2021-04-01 16:23:15 -0300

    Adding outer project for sever authentication

---
+++ algafood-auth/src/main/java/com/ffs/algafood/auth/AlgafoodAuthApplication.java
@@ -0,0 +1,12 @@
+package com.ffs.algafood.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgafoodAuthApplication.class, args);
+    }
+}
